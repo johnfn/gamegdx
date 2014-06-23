c429c9a4b6b29d223deadb28f95e32857b07d2f3
johnfn <johnfn@gmail.com>
2014-06-22 22:45:11 -0700

    Tiled maps working.

--- core/src/com/game/SuperGame.java
+++ core/src/com/game/SuperGame.java
@@ -3,27 +3,46 @@ package com.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 public class SuperGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-    int num = 0;
+    OrthographicCamera camera;
+    TiledMap map;
+    TiledMapRenderer mapRenderer;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+
+        map = new TmxMapLoader().load("map.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, width, height);
+        camera.update();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-        batch.draw(img, 0, 0);
-		batch.end();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        System.out.println(map.getLayers().get(0));
 	}
 
     public void test() {
